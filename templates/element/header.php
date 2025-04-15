a39bb5dc417f15e4cb9f9c0d8442bd8ab0f91c3a
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-04-15 17:29:35 +0200

    header finita (da aggiungere il filtro delle lingue)

--- templates/element/header.php
+++ templates/element/header.php
@@ -10,38 +10,14 @@ $languages = Configure::read('Setup.languages');
     <a href="<?= $homeLink ?>" class="header__logo">
         <?= $this->Frontend->svg('logo.svg') ?>
     </a>
-
-    <?php if (!empty($showSearch)): ?>
-        <div class="header__search">
-            <?php echo $this->element('search'); ?>
-        </div>
-    <?php endif; ?>
     
-    <?php if (empty($hamburgerMenu)): ?>
-        <nav class="header__menu">
-            <?= $this->cell('Menu.Menu', [1]) ?>
-        </nav>
-    <?php else: ?>
-        <div class="header__hamburger">
-            <?php echo $this->element('hamburger'); ?>
-        </div>
-        
-    <?php endif; ?>
-
+    <nav class="header__menu">
+        <?= $this->cell('Menu.Menu', [1]) ?>
+    </nav>
 
     <?php if (!empty($languages)): ?>
         <div class="header__languages">
             <?= $this->element('languages'); ?>
-        </div>
-    <?php endif; ?>
-
-    <?php if (!empty($cta)): ?>
-        <div class="header__cta">
-            <?= $this->element('cta', [
-                'label' => $cta['label'],
-                'icon' => $cta['icon'],
-                'url' => $cta['url']
-            ]); ?>
         </div>
     <?php endif; ?>
     
