d20cc004ed81e11001a433f7cf6eb2c0441fbaa7
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-04-15 13:23:06 +0200

    Creata prima parte

--- plugins/CustomPages/templates/CustomPages/home.php
+++ plugins/CustomPages/templates/CustomPages/home.php
@@ -1,3 +1,54 @@
-<h1><?= $item->title ?></h1>
+<div class="home">
+    
+    <!-- VIDEO -->
+    <div class="container-fluid home__video">
+        <video autoplay loop muted playsinline>
+            <source src="img/video/video1.mp4" type="video/mp4">
+            Il tuo browser non supporta il video.
+        </video>
+        <div class="home__video__content container-l m-auto">
+            <div class="home__video__title">
+                <h1 class="title-primary container-title">
+                    Tecnologia e ricerca per <span class="blue">soluzioni innovative</span>
+                </h1>
+            </div>
+            <div class="home__video__text">
+                <p class="font-23">
+                    Sistemi di aspirazione delle polveri e impianti 
+                    di depurazione delle acque reflue: tecnologia all'avanguardia per un mondo più sostenibile.
+                </p>
+                <div class="home__video__buttons">
+                    <?= $this->element('cta', [
+                        'extraClass' => 'button--primary',
+                        'url' => '#',
+                        'label' => 'Aspirazione polveri',
+                        'icon' => 'icons/button.svg'
+                    ]); ?>
+                    <?= $this->element('cta', [
+                        'extraClass' => 'button--primary',
+                        'url' => '#',
+                        'label' => 'Depurazione acque',
+                        'icon' => 'icons/button.svg'
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<?= $item->text_1 ?>
+    <!-- SETTORI DI IMPIEGO -->
+    <div class="home__settori bg-light">
+        <div class="container-l m-auto">
+            <div class="home__settori__title font-23 blue">
+                Settori d'impiego
+            </div>
+            <div class="home__settori__content">
+                <ul class="font-86 blue">
+                    <li>Cemento</li>
+                    <li>Marmi, graniti e pietre</li>
+                    <li>Vetro e derivati</li>
+                    <li>Ceramica</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- templates/element/cta.php
+++ templates/element/cta.php
@@ -1,25 +1,25 @@
 <?php if (empty($label)) return; ?>
 
 <?php if (!empty($url)) : ?>
-    <a class="cta <?= $extraClass ?? '' ?>" href="<?= $url ?>">
-        <span class="cta__label">
-            <?= $label ?>
-        </span>
+    <a class="cta" href="<?= $url ?>">
         <?php if (!empty($icon)) : ?>
-            <span class="cta__icon">
+            <span class="button <?= $extraClass ?? '' ?>">
                 <?= $this->Frontend->svg($icon) ?>
             </span>
         <?php endif; ?>
+        <span class="cta__label">
+            <?= $label ?>
+        </span>
     </a>
 <?php else : ?>
-    <button class="cta <?= $extraClass ?? '' ?>" type="submit">
-        <span class="cta__label">
-            <?= $label ?>
-        </span>
+    <button class="cta" type="submit">
         <?php if (!empty($icon)) : ?>
-            <span class="cta__icon">
+            <span class="button <?= $extraClass ?? '' ?>">
                 <?= $this->Frontend->svg($icon) ?>
             </span>
         <?php endif; ?>
+        <span class="cta__label">
+            <?= $label ?>
+        </span>
     </button>
 <?php endif; ?>
