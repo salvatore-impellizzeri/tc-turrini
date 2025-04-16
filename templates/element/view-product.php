da0f41a6dc758c6fdbe64f37d9fb801aac9ac1c2
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-04-16 14:47:18 +0200

    finita view

--- templates/element/view-product.php
+++ templates/element/view-product.php
@@ -1,13 +1,33 @@
 <div class="view-product">
-    <div>
-        <div>
+    <div class="container-l m-auto">
+        <div class="font-14">
             Prodotti > <?= $section ?>
         </div>
         <h1 class="title-primary">
             <?= $product ?>
         </h1>
-        <div class="view-prodcut__items">
-            
+        <div class="view-product__items">
+            <?php foreach ($items as $item) { ?>
+                <div class="view-product__item">
+                    <div class="view-product__img">
+                        <img src="<?= $item['img'] ?>" alt="<?= $item['label'] ?>">
+                    </div>
+                    <div class="view-product__text">
+                        <h3 class="font-35"><?= $item['label'] ?></h3>
+                        <p class="font-18"><?= $item['text'] ?></p>
+                        <div class="font-14 view-product__buttons">
+                            <button>
+                                <?= $this->Frontend->svg('icons/download.svg'); ?>
+                                <span>Scarica la scheda tecnica</span>
+                            </button>
+                            <button>
+                                <?= $this->Frontend->svg('icons/more.svg') ?>
+                                <span>Maggiori dettagli</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </div>
