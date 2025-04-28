529f3244eab3c190e9e0a911648983a0cd19b672
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-04-28 16:55:55 +0200

    fixate alcune cose in responsive

--- templates/element/list-products.php
+++ templates/element/list-products.php
@@ -9,24 +9,12 @@
     <div class="container-l m-auto">
         <div class="list-products__items">
         <?php
-            $total = count($items);
-            foreach ($items as $index => $item) {
-                // Se è il 3°, 6°, 9°, ecc. (ogni terzo elemento)
-                $isLastInRow = (($index + 1) % 3 === 0);
-
-                // Se è uno degli ultimi elementi e siamo a fine lista
-                if (!$isLastInRow && $index === $total - 1) {
-                    $isLastInRow = true;
-                }
-
-                $extraClass = $isLastInRow ? 'last-in-row' : '';
-            ?>
-            <div>
+            foreach ($items as $index => $item) { ?>
+            <div class="list-products__item">
                 <?= $this->element('product-preview', [
                     'label' => $item['label'],
                     'img' => $item['img'],
                     'url' => $item['url'],
-                    'extraClass' => $extraClass,
                 ]); ?>
             </div>
         <?php } ?>
